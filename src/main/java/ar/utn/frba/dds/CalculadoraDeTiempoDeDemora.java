4dabba9abcbdb8ac1495037b8867d9b2cb3d437e
MatiasCanevaro <mcanevaro@frba.utn.edu.ar>
2024-05-04 19:24:00 -0300

    agrego punto 2

---
+++ src/main/java/ar/utn/frba/dds/CalculadoraDeTiempoDeDemora.java
@@ -0,0 +1,26 @@
+package ar.utn.frba.dds;
+
+public class CalculadoraDeTiempoDeDemora
+{
+    private CalculadoraDeDistancia calculadoraDeDistancia;
+    public Integer calcularTiempoDeDemoraAproximadoEnMinutos(List<Destino> paradas){
+        Double distanciaEnKMTotal = 0.0;
+        Integer tiempoDetenidoEnMinutos = 0;
+
+        for(int i=0;i<paradas.size()-1;i++){
+            Destino destinoInicial = paradas.get(i);
+            Destino destinoFinal = paradas.get(i+1);
+            distanciaEnKMTotal += calculadoraDeDistancia.calcularDistanciaEnKMEntre(destinoInicial,destinoFinal);
+            if(destinoInicial.cantidadMinutosADetenerse()>0){
+                tiempoDetenidoEnMinutos += destinoInicial.cantidadMinutosADetenerse();
+            }
+        }
+
+        Integer velocidadPromedioEnKMPorHora = 4;
+        Double tiempoTotalEnHoras = distanciaEnKMTotal/velocidadPromedioEnKMPorHora;
+        Integer tiempoTotalEnMinutos = (int) (tiempoTotalEnHoras*60); //convertimos horas a minutos
+        tiempoTotalEnMinutos += tiempoDetenidoEnMinutos;
+
+        return tiempoTotalEnMinutos;
+    }
+}
